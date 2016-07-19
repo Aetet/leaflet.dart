3a727e8d953c42016fb2c5baa7aaa1a943baab40
Max Grebenschikov <maxim.grebenshikov@team.wrike.com>
2016-07-19 23:50:20 +0300

    Removelayer

--- lib/leaflet.dart
+++ lib/leaflet.dart
@@ -51,6 +51,10 @@ class LeafletMap {
     _map.callMethod('addLayer', [layer.layer]);
   }
 
+  void removeLayer(Layer layer) {
+    _map.callMethod('removeLayer', [layer.layer]);
+  }
+
   void eachLayer(f) {
     _map.callMethod('eachLayer', [allowInterop(f)]);
   }
